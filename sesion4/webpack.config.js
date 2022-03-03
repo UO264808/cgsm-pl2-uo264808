5a89dc996ad285710918f91892b6c86c5959bbfe
LucasP <uo264808@uniovi.es>
2022-03-03 14:49:00 +0100

    finished session 4

---
+++ sesion4/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    mode: "production",
+    entry: {
+        "prac4-1": './src/prac4-1.js',
+        "prac4-2": './src/prac4-2.js',
+        "prac4-3": './src/prac4-3.js',
+        "prac4-4": './src/prac4-4.js'
+    },
+    devServer: {
+        static: {
+            directory: __dirname
+        },
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000
+    }
+};
